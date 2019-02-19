957486724cc3687d41d0bde60f60f55384feb5cd
jacob-salamy <39494067+jacob-salamy@users.noreply.github.com>
2019-02-18 23:27:20 -0500

    Update Tournament.R

--- Tournament.R
+++ Tournament.R
@@ -1,4 +1,4 @@
-# Tournament for Rossmann Data #
+# Tournament for rossmann Data #
 
 library(forecast)
 library(data.table)
@@ -7,20 +7,24 @@ library(parallel)
 input <- fread("~/Desktop/Forecasting/rossmann-store-sales/train.csv")
 input <- input[, c(1,3,4)]
 
+# Format data
 input[, Date := as.Date(Date)]
 input <- input[order(Store, Date)]
 input[, Sales := ts(Sales), by = Store]
 
+# Define forecast horizon and validation window
 forecast <- 30
 validation <- 30
 start <- max(input$Date) - validation
 
+# Define different models 
 models <- list(
   arima  = function(data, period) auto.arima(data) %>% forecast(., h = period) %>% .[['mean']] %>% as.vector(.),
   ses    = function(data, period) ses(data, h = period)[['mean']] %>% as.vector(.),
   croston = function(data, period) croston(data, h = period)[['mean']] %>% as.vector(.)
   )
 
+# Function that finds and returns results from most accurate forecasting method over the validation window
 Tournament <- function(input, models){
   Score <- vector('list', length = length(models))
   for (i in seq_along(models)){
@@ -36,6 +40,7 @@ Tournament <- function(input, models){
   return(Forecast)
 }
 
+# Run the Tournament function in parallel
 Forecasts <- split(input, input$Store) %>% mclapply(., Tournament, models, mc.cores = 2) %>% rbindlist(.)
 
 names(Forecasts)[1] <- 'Forecast'
