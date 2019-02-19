b975fc771d6dd915d8b3b72a39cc078248706233
jacob-salamy <39494067+jacob-salamy@users.noreply.github.com>
2019-02-19 08:57:09 -0500

    Update Tournament.R

--- Tournament.R
+++ Tournament.R
@@ -6,6 +6,7 @@ library(parallel)
 
 input <- fread("~/Desktop/Forecasting/rossmann-store-sales/train.csv")
 input <- input[, c(1,3,4)]
+input <- input[Store %in% unique(Store)[1:5],]
 
 # Format data
 input[, Date := as.Date(Date)]
@@ -15,26 +16,27 @@ input[, Sales := ts(Sales), by = Store]
 # Define forecast horizon and validation window
 forecast <- 28
 validation <- 14
-start <- max(input$Date) - validation - forecast
+valid_start <- max(input$Date) - validation - forecast
+valid_end <- max(input$Date) - forecast
 
 # Define different models 
 models <- list(
   arima  = function(data, period) auto.arima(data) %>% forecast(., h = period) %>% .[['mean']] %>% as.vector(.),
   ses    = function(data, period) ses(data, h = period)[['mean']] %>% as.vector(.),
   croston = function(data, period) croston(data, h = period)[['mean']] %>% as.vector(.)
-  )
+)
 
 # Function that finds and returns results from most accurate forecasting method over the validation window
 Tournament <- function(input, models){
   Score <- vector('list', length = length(models))
   for (i in seq_along(models)){
-  Score[[i]] <- sum(abs(input$Sales[input$Date >= start & input$Date < start + validation] - 
-                          models[[i]](data = input$Sales[input$Date < start], period = validation)))
+    Score[[i]] <- sum(abs(input$Sales[input$Date >= valid_start & input$Date < valid_end] - 
+                            models[[i]](data = input$Sales[input$Date < valid_start], period = validation)))
   }
   Score <- unlist(Score) %>% t(.)
   names(Score) <- names(models)
   Winner <- names(Score)[Score[1,] == min(Score[1,])]
-  Forecast <- models[[Winner]](data = input$Sales[input$Date <= start + validation],
+  Forecast <- models[[Winner]](data = input$Sales[input$Date <= valid_end],
                                period = forecast) %>% as.data.frame(.)
   Forecast$Model <- Winner
   return(Forecast)
@@ -44,5 +46,5 @@ Tournament <- function(input, models){
 Forecasts <- split(input, input$Store) %>% mclapply(., Tournament, models, mc.cores = 2) %>% rbindlist(.)
 
 names(Forecasts)[1] <- 'Forecast'
-Forecasts$Date <- input[Date > start + validation, Date]
-Forecasts$Store <- input[Date > start + validation, Store]
+Forecasts$Date <- input[Date > valid_end, Date]
+Forecasts$Store <- input[Date > valid_end, Store]
